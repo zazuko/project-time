1783bef51a258518541ed7d5fa3885b173c19dd0
Reto Gmür <reto@apache.org>
2015-09-28 23:52:28 +0200

    started entry-adding

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,15 +1,16 @@
+/* global LD2h, rdf */
 
 $(function () {
-    var prefix = "http://example.org/project/";
+    var prefix = "http://example.org/";
     var graphname = "http://zazuko.com/projects";
     var store = new rdf.SparqlStore({
         endpointUrl: $("#sparqlQuery").val(),
         updateUrl: $("#sparqlUpdate").val() //shouldn't be neede
-    })
+    });
 
     LD2h.getDataGraph = function (callback) {
         store.graph(graphname, callback); //Caveat: with newer rdf-ext version the callback takes (uri,g)
-    }
+    };
     $("#setEndpoints").click(function (e) {
         $("#endpointsForm").hide();
         $("#endpointsLink").show();
@@ -18,26 +19,37 @@ $(function () {
         $("#endpointsLink").hide();
         $("#endpointsForm").show();
     });
+    var sparqlUpdate = function(update) {
+        jQuery.post($("#sparqlUpdate").val(), {update: update})
+                .done(function () {
+                    console.log("successfully updated");
+                    window.location.reload();
+                })
+                .fail(function (e) {
+                    console.log("error with query " + update, e);
+                    alert("error (see console for details)");
+                });
+    }
     $("#createProject").on("click", function () {
         var projectTitle = $("#projectTitle").val();
         if (projectTitle.length < 4) {
-            alert("Project title too short.")
+            alert("Project title too short.");
             return;
         }
-        var shortName = $("#projectName").val()
+        var shortName = $("#projectName").val();
         if (shortName.length < 3) {
-            alert("Project short name must not be shorter than 3 characters.")
+            alert("Project short name must not be shorter than 3 characters.");
             return;
         }
         if (shortName.indexOf(' ') > -1) {
-            alert("Project short name must not contain spaces.")
+            alert("Project short name must not contain spaces.");
             return;
         }
         var projectHours = $("#projectHours").val();
         if (!projectHours) {
             projectHours = 0;
         }
-        var projectIRI = prefix + shortName;
+        var projectIRI = prefix + "project/"+shortName;
         var update = "PREFIX dct: <http://purl.org/dc/terms/>\n" +
                 "PREFIX pro: <http://schema.zazukoians.org/projects/>\n" +
                 "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
@@ -47,16 +59,35 @@ $(function () {
                 "<" + projectIRI + ">  pro:plannedTime  " + projectHours + "." +
                 "<" + projectIRI + "> dct:title  \"" + projectTitle + "\" } \n" +
                 "}";
-        jQuery.post($("#sparqlUpdate").val(), {update: update})
-                .done(function () {
-                    console.log("successfully updated");
-                    window.location.reload()
-                })
-                .fail(function (e) {
-                    console.log("error with query " + update, e)
-                    alert("error (see console for details)");
-                });
-    })
-    RDF2h.prefixMap["pro"] = "http://schema.zazukoians.org/projects/";
+        sparqlUpdate(update);
+    });
+    $("#reportHours").on("click", function () {
+        function makeid() {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for( var i=0; i < 7; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        };
+        var entryHours = $("#entryHours").val();
+        if (!entryHours) {
+            alert("Must set entry hours");
+            return;
+        }
+        var entryIri = prefix+"entry/"+makeid();
+        var update = "PREFIX dct: <http://purl.org/dc/terms/>\n" +
+                "PREFIX pro: <http://schema.zazukoians.org/projects/>\n" +
+                "PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+                "INSERT DATA\n" +
+                "{ GRAPH <" + graphname + ">\n" +
+                "{ <" + entryIri + ">  rdf:type  pro:Entry." +
+                "<" + entryIri + ">  pro:spentTime  " + entryHours + "." +
+                "<" + entryIri + "> pro:entryDate  \"" + $("#entryDate").val() + "\"^^xsd:date } \n" +
+                "}";
+        sparqlUpdate(update);
+    });
     LD2h.expand();
 });
